02f2748621152fbd873e3423e721c45c6a46029b
mostafa H <mostafaheidary@hotmail.com>
2018-01-25 14:06:50 +0330

    First Commit.

---
+++ PriorityHandler/PriorityHandler/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriorityHandler
+{
+    public class PriorityItem
+    {
+        #region fields
+        public int priority = 0;
+        #endregion end of fields
+    }
+}
